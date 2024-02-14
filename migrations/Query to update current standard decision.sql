d0ce9eb156a2a495833c66a58018febc6cc23556
ADAM M SANADI <64148954+adamsanadi6@users.noreply.github.com>
2024-02-14 10:37:03 +0530

    Update Query to update current standard decision.sql

--- migrations/Query to update current standard decision.sql
+++ migrations/Query to update current standard decision.sql
@@ -4,6 +4,8 @@ set role adsruat;
 --we retrieve the latest details from the 'In which standard he/she is studying?' question in the 'Annual Visit - Baseline' program encounter and 
 --then update the 'Current Standard' in the decision encounter.
 
+-- If you intend to make changes in the production environment, please modify the organization name and last modified by id.
+
 WITH baseline_encounters AS (
     SELECT 
         id,
@@ -15,7 +17,9 @@ WITH baseline_encounters AS (
         AND pe.encounter_date_time IS NOT NULL 
 )
 UPDATE public.program_enrolment AS enrol
-SET observations = enrol.observations || jsonb_build_object('67b7a434-fdc7-44b6-89e7-b4755afd0bc3', enc.obs -> '2424293e-2466-4122-970f-716f3019ad55')
+SET observations = enrol.observations || jsonb_build_object('67b7a434-fdc7-44b6-89e7-b4755afd0bc3', enc.obs -> '2424293e-2466-4122-970f-716f3019ad55'), 
+    last_modified_date_time = current_timestamp + ((enrol.id % 1000) * interval '1 millisecond'),
+    last_modified_by_id = 10366;
 FROM baseline_encounters AS enc
 WHERE enc.program_enrolment_id = enrol.id
     AND enc.visit_number = 1

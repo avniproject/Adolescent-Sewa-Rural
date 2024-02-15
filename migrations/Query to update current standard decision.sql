f0459ae35a33e7124376b2e0e68aac516110d7ba
ADAM M SANADI <64148954+adamsanadi6@users.noreply.github.com>
2024-02-15 15:06:37 +0530

    Update Query to update current standard decision.sql

--- migrations/Query to update current standard decision.sql
+++ migrations/Query to update current standard decision.sql
@@ -5,7 +5,7 @@ set role adsruat;
 --then update the 'Current Standard' in the decision encounter.
 
 -- If you intend to make changes in the production environment, please modify the organization name and last modified by id.
-
+   
 WITH baseline_encounters AS (
     SELECT 
         id,
@@ -17,13 +17,19 @@ WITH baseline_encounters AS (
         AND pe.encounter_date_time IS NOT NULL 
 )
 UPDATE public.program_enrolment AS enrol
-SET observations = enrol.observations || jsonb_build_object('67b7a434-fdc7-44b6-89e7-b4755afd0bc3', enc.obs -> '2424293e-2466-4122-970f-716f3019ad55'), 
+SET observations = 
+    CASE 
+        WHEN (enc.obs -> '9705f6ad-50e1-4179-aa60-922014d7cc3c')::TEXT = '"995cfaee-5598-4293-addc-dcb1da5dbcd3"' THEN 
+            enrol.observations || jsonb_build_object('67b7a434-fdc7-44b6-89e7-b4755afd0bc3', enc.obs -> '2424293e-2466-4122-970f-716f3019ad55')
+        WHEN (enc.obs -> '9705f6ad-50e1-4179-aa60-922014d7cc3c')::TEXT = '"fb1080b4-d1ec-4c87-a10d-3838ba9abc5b"' OR 
+             (enc.obs -> '9705f6ad-50e1-4179-aa60-922014d7cc3c') IS NULL THEN 
+            enrol.observations || jsonb_build_object('67b7a434-fdc7-44b6-89e7-b4755afd0bc3', '"fb1080b4-d1ec-4c87-a10d-3838ba9abc5b"')
+        ELSE enrol.observations
+    END,
     last_modified_date_time = current_timestamp + ((enrol.id % 1000) * interval '1 millisecond'),
     last_modified_by_id = 10366,
     manual_update_history = manual_update_history || ', Updating the program decision concept Current Standard for old data as per #24 card'
 FROM baseline_encounters AS enc
 WHERE enc.program_enrolment_id = enrol.id
     AND enc.visit_number = 1
-    AND enrol.program_id = 975
-    AND (enc.obs -> '9705f6ad-50e1-4179-aa60-922014d7cc3c')::TEXT = '"995cfaee-5598-4293-addc-dcb1da5dbcd3"'
-    AND enrol.observations -> '67b7a434-fdc7-44b6-89e7-b4755afd0bc3' is null;
+    AND enrol.program_id = 975;

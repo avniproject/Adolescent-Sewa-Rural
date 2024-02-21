1dc02d83edfce0f6bd56ff9bf0003f5e5da569aa
ADAM M SANADI <64148954+adamsanadi6@users.noreply.github.com>
2024-02-21 10:19:23 +0530

    Update Query to update the program exit details for individuals.sql

--- migrations/Query to update the program exit details for individuals.sql
+++ migrations/Query to update the program exit details for individuals.sql
@@ -17,21 +17,18 @@ set
 	program_exit_observations = enrl.program_exit_observations || jsonb_build_object('55454f19-f84e-4be4-afe8-c20a6ed6576a', '"5c74a9c0-659e-4915-b8fd-d0203da0d3a0"'),  
 	last_modified_date_time = '2024-01-01'::timestamp + ((enrl.id % 1000) * interval '1 millisecond'),
 	program_exit_date_time = '2024-01-01'::timestamp + ((enrl.id % 1000) * interval '1 millisecond'),
-	last_modified_by_id = 10366,
-	manual_update_history = enrl.manual_update_history || ', Updating the program decision concept Current Standard for old data as per #23 card'
+	last_modified_by_id = 10365,
+	manual_update_history = append_manual_update_history(enrl.manual_update_history, ' Updating the program decision concept Current Standard for old data as per #23 card')
 from
 	public.individual i
-left join address a on
+left join adsr.address a on
 	a.id = i.address_id
-left join public.address_level al on
-	al.id = a."School id"
 where
 	i.is_voided = false
 	and enrl.is_voided = false
 	and i.id = enrl.individual_id
-	and al.type_id = 715 -- al.type_id = 1307 for UAT
 	and enrl.program_exit_date_time is null
-	and al.title in ('Rajpardi Kanya Primary', 'Jhagadiya branch primary', 'Zagadiya Kumar Primary', 'Ranipura P.S.', 'Limbodara Primary');
+	and a."School" in ('Rajpardi Kanya Primary', 'Jhagadiya branch primary', 'Zagadiya Kumar Primary', 'Ranipura P.S.', 'Limbodara Primary');
 
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -41,21 +38,19 @@ select
 	i.id,
 	i.first_name,
 	i.last_name,
-	al.title as "School",
+	a."School" as "School",
 	enrl.id as enrl_id,
-        '2024-01-01'::timestamp + ((enrl.id % 1000) * interval '1 millisecond') as time,
+	'2024-01-01'::timestamp + ((enrl.id % 1000) * interval '1 millisecond') as time,
+	append_manual_update_history(enrl.manual_update_history, ' Updating the program decision concept Current Standard for old data as per #23 card') as history,
 	enrl.program_exit_observations
 from
-	program_enrolment enrl
-left join individual i on
+	public.program_enrolment enrl
+left join public.individual i on
 	i.id = enrl.individual_id
-left join address a on
+left join adsr.address a on
 	a.id = i.address_id
-left join address_level al on
-	al.id = a."School id"
 where
 	i.is_voided = false
 	and enrl.is_voided = false
-	and al.type_id = 715
 	and enrl.program_exit_date_time is null
-	and al.title in ('Rajpardi Kanya Primary', 'Jhagadiya branch primary', 'Zagadiya Kumar Primary', 'Ranipura P.S.', 'Limbodara Primary');
+	and a."School" in ('Rajpardi Kanya Primary', 'Jhagadiya branch primary', 'Zagadiya Kumar Primary', 'Ranipura P.S.', 'Limbodara Primary');
